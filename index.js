a4d896b61c7326d80b3b73941129d867e2f94190
Demarius Chrite <chrite.demarius@gmail.com>
2013-07-31 17:11:26 -0400

    Proposers keep track of current accepted value.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function Proposer (n) {
     this._promised = []
     this._acceptors = []
     this.prepared = 0
+    this.value = null
 }
 
 Proposer.prototype.addAcceptor = function (acceptor) {
@@ -20,6 +21,7 @@ Proposer.prototype.prepare = function (proposal) {
             this._promised.push(i)
         } else {
             this.proposal_num = req.highest_proposal_num + 1
+            this.value = req.value
         }
     }
     if (this._promised.length > (this._promised.length / 2)) {
@@ -29,7 +31,7 @@ Proposer.prototype.prepare = function (proposal) {
 
 Proposer.prototype.propose = function () {
     for (var i in this._promised) {
-        this._acceptors[i].accept(this.proposal, this)
+        this.value = this._acceptors[i].accept(this.proposal, this)
     }
 }
 
@@ -64,8 +66,13 @@ Acceptor.prototype.accept = function (value, sender) {
     //  possible that another proposer could hijack our
     //  proposal number somehow
     if (this.promised == sender) {
+        this.message = value
         this._send(value)
+        return value
+    } else {
+        return this.message
     }
+
 }
 
 Acceptor.prototype.addLearner = function (learner) {
@@ -73,13 +80,13 @@ Acceptor.prototype.addLearner = function (learner) {
 }
 
 Acceptor.prototype._send = function (value) {
-    this.message = value
     this.learners.forEach(function (learner) {
         learner._set(value, this.highest_proposal_num)
     })
 }
 
 Acceptor.prototype._set = function (value, n) {
+    this.promised = null
     this.message = value
     this.highest_proposal_num = n
 }
