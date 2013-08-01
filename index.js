dc3ef39f62d5ab49f7047ea30e3ece1544759098
Demarius Chrite <chrite.demarius@gmail.com>
2013-08-01 11:08:19 -0400

    Working model of proposers/acceptors with test.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function Proposer (n) {
+function Proposer (n, id) {
+    this.id = id
     this.proposal_num = n
     this.proposal = null
     this._promised = []
@@ -24,26 +25,28 @@ Proposer.prototype.prepare = function (proposal) {
             this.value = req.value
         }
     }
-    if (this._promised.length > (this._promised.length / 2)) {
-        propose()
+    if (this._promised.length > (this._acceptors.length / 2)) {
+        this.propose()
     }
 }
 
 Proposer.prototype.propose = function () {
     for (var i in this._promised) {
-        this.value = this._acceptors[i].accept(this.proposal, this)
+        this.value = this._acceptors[i].accept(this.proposal, this.id)
+        this._promised.splice(i, 1)
+        this.proposal_num += 1
     }
 }
 
 function Acceptor (n) {
-    this.highest_proposal = n
+    this.highest_proposal_num = n
     this.message = null
     this.learners = []
     this.promised = null
 }
 
 Acceptor.prototype.prep = function (n, value, sender) {
-    if (n > this.highest_proposal) {
+    if (n > this.highest_proposal_num) {
         this.highest_proposal_num = n
         this.promised = sender
 
@@ -62,10 +65,11 @@ Acceptor.prototype.prep = function (n, value, sender) {
 }
 
 Acceptor.prototype.accept = function (value, sender) {
-    //  ensure this is the sender we promised to wait for
-    //  possible that another proposer could hijack our
+    //  ensure this is the sender we promised to wait for.
+    //  Possible that another proposer could hijack our
     //  proposal number somehow
     if (this.promised == sender) {
+        console.log('accepted ' + value)
         this.message = value
         this._send(value)
         return value
@@ -93,4 +97,12 @@ Acceptor.prototype._set = function (value, n) {
 
 
 exports.paxos = function (messages) {
+    var proposer = new Proposer(1, 100), acceptor = new Acceptor(0)
+    proposer.addAcceptor(acceptor)
+
+    messages.forEach(function (message) {
+        proposer.prepare(message)
+    })
+
+    return acceptor.message
 }
---
+++ t/paxos/propose.t.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+require('proof')(1, function (equal, say) {
+    var paxos = require('../..').paxos
+    var messages = [
+        "the soup",
+        "the dance",
+        "the troops",
+        "the fans"
+    ]
+
+    paxos = paxos(messages)
+
+    equal(paxos, messages[3], 'match')
+    say(paxos)
+})
