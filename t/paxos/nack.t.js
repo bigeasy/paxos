5f06a6188bf060ee534a1c7d5fd91778d4bf9571
Demarius Chrite <chrite.demarius@gmail.com>
2014-07-16 21:44:35 -0400

    Create 'nack.t.js'.

--- t/paxos/nack.t.js
+++ t/paxos/nack.t.js
@@ -1,29 +1,5 @@
 #!/usr/bin/env node
-require('proof')(1, function(equal) {
-    var paxos = require('../../index.js')
-    var nodes = []
 
-    var generateProposalId = function (n) {
-        if (n) return n+1;
-        return 1;
-    }
-
-    var cluster = new paxos.Cluster(nodes)
-
-    for (var i=0; i<5; i++) {
-        nodes[i] = new paxos.Node(i, '127.0.0.1', 80+i, generateProposalId, 1)
-        if (i < 1) {
-            paxos.initializeProposer(nodes[i], cluster)
-        } else {
-            paxos.initializeAcceptor(nodes[i], cluster)
-        }
-
-        if (i == 2) {
-            nodes[i].promisedId = 20
-            nodes[0].startProposal("jump")
-        }
-    }
-
-
-    equal(nodes[0].proposalId, 21, 'NACK received') // TODO: ask Alan how to do the thing
-})
+require('proof')(1, function (step) {
+    // send NACK
+}
