5dfef340b45bc2bdb3fbf3b53eb4e79290fefe9a
Demarius Chrite <chrite.demarius@gmail.com>
2014-07-17 21:39:12 -0400

    Work on NACK test.

--- t/paxos/nack.t.js
+++ t/paxos/nack.t.js
@@ -1,5 +1,32 @@
 #!/usr/bin/env node
 
-require('proof')(1, function (step) {
+require('proof')(1, function (equal) {
     // send NACK
-}
+    var paxos = require('../../index.js')
+    var socket = require('dgram').createSocket("udp4")
+    socket.bind(1024, '0.0.0.0')
+
+    var generateProposalId = function (n) {
+        if (n) return n+1;
+        return 1;
+    }
+
+    var nodes = []
+    var cluster = new paxos.Cluster(nodes)
+
+    nodes[0] = new paxos.Node(1, '0.0.0.0', 1025, generateProposalId, 1)
+    paxos.initializeProposer(nodes[0], cluster)
+    nodes[0].startProposal("test")
+
+    var nack = new Buffer(JSON.stringify({
+        type: "NACK",
+        address: '0.0.0.0',
+        port: 1026,
+        highestProposalNum: 2
+    }))
+    socket.send(nack, 0, nack.length, 1025, '0.0.0.0', function () {
+      setTimeout(function () {
+          equal(nodes[0].proposalId, 3, "proposal ID raised")
+      }, 4000)
+    })
+})
