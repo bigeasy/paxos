6712a2cad3a1d01e8f25ee6908d08441733b9919
Alan Gutierrez <alan@prettyrobots.com>
2015-01-03 22:16:22 -0600

    Move `Id` to it's own module.
    Closes #219.

--- legislator.js
+++ legislator.js
@@ -7,43 +7,10 @@ var RBTree = require('bintrees').RBTree
 var Cache = require('magazine')
 
 var consume = require('./consume')
+var Id = require('./id')
 
 function random (min, max) {
     return Math.floor(Math.random() * (max - min)) + min
-}
-
-var Id = {
-    toWords: function (id) {
-        var split = id.split('/')
-        return [ Monotonic.parse(split[0]), Monotonic.parse(split[1]) ]
-    },
-    toString: function (id) {
-        return Monotonic.toString(id[0]) + '/' + Monotonic.toString(id[1])
-    },
-    compare: function (a, b, index) {
-        a = Id.toWords(a)
-        b = Id.toWords(b)
-        if (index == null) {
-            var compare = Monotonic.compare(a[0], b[0])
-            if (compare == 0) {
-                return Monotonic.compare(a[1], b[1])
-            }
-            return compare
-        }
-        return Monotonic.compare(a[index], b[index])
-    },
-    increment: function (id, index) {
-        id = Id.toWords(id)
-        var next = [ id[0], id[1] ]
-        next[index] = Monotonic.increment(next[index])
-        if (index == 0) {
-            next[1] = [ 0 ]
-        }
-        return Id.toString(next)
-    },
-    isGovernment: function (id) {
-        return Id.compare('0/0', id, 1) == 0
-    }
 }
 
 function Legislator (id, options) {
