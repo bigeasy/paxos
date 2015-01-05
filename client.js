f6b0265fdd35e920343a616e757b906bd853673e
Alan Gutierrez <alan@prettyrobots.com>
2015-01-05 14:31:14 -0600

    Implement client queue.
    Closes #136.

---
+++ client.js
@@ -0,0 +1,150 @@
+var assert = require('assert')
+var Monotonic = require('monotonic')
+var RBTree = require('bintrees').RBTree
+var Id = require('./id')
+var unshift = [].unshift
+
+function Client (id) {
+    this.id = id
+    this.cookie = id + '/' + 0
+    this.sent = { ordered: [], indexed: {} }
+    this.pending = { ordered: [], indexed: {} }
+    this.log = new RBTree(function (a, b) { return Id.compare(a.promise, b.promise) })
+}
+
+Client.prototype.publish = function (value) {
+    var cookie = this.nextCookie()
+    var request = { cookie: cookie, value: value }
+    this.pending.ordered.push(request)
+    this.pending.indexed[cookie] = request
+    return cookie
+}
+
+Client.prototype.nextCookie = function () {
+    return this.cookie = Id.increment(this.cookie, 1)
+}
+
+Client.prototype.outbox = function () {
+    var outbox = []
+    if (this.flush) {
+        outbox = [{ cookie: this.nextCookie(), value: 0 }]
+    } else if (!this.boundary && this.sent.ordered.length == 0 && this.pending.ordered.length != 0) {
+        this.sent = this.pending
+        outbox = this.sent.ordered.map(function (request) {
+            return { cookie: request.cookie, value: request.value }
+        }, this)
+        this.pending = { ordered: [], indexed: {} }
+    }
+    return outbox
+}
+
+Client.prototype.published = function (receipts) {
+    if (receipts == null) {
+        this.flush = true
+    } else if (this.flush) {
+        assert(receipts.length == 1, 'too many receipts')
+        this.flush = false
+        this.boundary = receipts[0]
+    } else {
+        receipts.forEach(function (receipt) {
+            assert(!this.sent.indexed[receipt.cookie].promise, 'duplicate receipt')
+            this.sent.indexed[receipt.cookie].promise = receipt.promise
+        }, this)
+    }
+    delete this.sent.indexed
+}
+
+Client.prototype.prime = function (entries) {
+    this._ingest(entries)
+    this.uniform = this.log.min().promise
+    this.log.min().uniform = true
+    this.playUniform()
+}
+
+Client.prototype.retry = function () {
+    unshift.apply(this.pending.ordered, this.sent.ordered)
+    this.sent.ordered.forEach(function (request) {
+        this.pending.indexed[request.cookie] = request
+    }, this)
+    this.sent.ordered.length = 0
+}
+
+Client.prototype.playUniform = function (entries) {
+    var start = this.uniform, iterator = this.log.findIter({ promise: start }),
+        previous, current,
+        request
+
+    for (;;) {
+        previous = iterator.data(), current = iterator.next()
+        if (!current) {
+            break
+        }
+        current.uniform = current.previous == previous.promise
+        if (!current.uniform) {
+            break
+        }
+        this.uniform = current.promise
+        var request = this.sent.ordered[0] || {}, boundary = this.boundary
+        if (request.cookie == current.cookie) {
+            assert(request.promise == null
+                || Id.compare(request.promise, current.promise) == 0, 'cookie/promise mismatch')
+            this.sent.ordered.shift()
+        } else if (messagesLost.call(this)) {
+            if (Id.isGovernment(current.promise) && current.value && current.value.remap) {
+                if (this.boundary) {
+                    var mapping = current.value.remap.filter(function (mapping) {
+                        return this.boundary.promise == mapping.was
+                    }, this).shift()
+                    assert(mapping, 'remap did not include posted boundary')
+                    this.boundary.promise = mapping.is
+                } else {
+                    var remapped = []
+                    current.value.remap.forEach(function (mapping) {
+                        if (this.sent.ordered.length && mapping.was == this.sent.ordered[0].promise) {
+                            var request = this.sent.ordered.shift()
+                            request.promise = mapping.is
+                            remapped.push(request)
+                        }
+                    }, this)
+                    assert(this.sent.ordered.length == 0, 'remap did not remap all posted entries')
+                    this.sent.ordered = remapped
+                }
+            } else {
+                this.retry()
+            }
+            delete this.boundary
+        }
+    }
+
+    function messagesLost () {
+        return (boundary && Id.compare(current.promise, boundary.promise) >= 0) ||
+               (request.promise && Id.compare(current.promise, request.promise) > 0)
+    }
+
+    return start
+}
+
+Client.prototype._ingest = function (entries) {
+    entries.forEach(function (entry) {
+        var found = this.log.find({ promise: entry.promise })
+        if (!found) {
+            this.log.insert(entry)
+        }
+    }, this)
+}
+
+Client.prototype.receive = function (entries) {
+    this._ingest(entries)
+    return this.playUniform()
+}
+
+Client.prototype.each = function (marker, callback) {
+    var iterator = this.log.findIter({ promise: marker })
+    assert(iterator, 'promise not found')
+    var entry
+    while ((entry = iterator.next()) && entry.uniform) {
+        callback(entry)
+    }
+}
+
+module.exports = Client
---
+++ t/paxos/client.t.js
@@ -0,0 +1,180 @@
+
+require('proof')(27, prove)
+
+function prove (assert) {
+    var Client = require('../../client')
+
+    var client = new Client('0')
+
+    client.prime([{ value: null, promise: '1/0' }])
+    var marker = client.receive([
+        { cookie: '2/0', promise: '1/1', value: 1, previous: '1/0' },
+    ])
+
+    assert(client.outbox(), [], 'outbox is empty')
+    assert(client.outbox(), [], 'outbox is still empty')
+    assert(client.outbox(), [], 'outbox is ever still empty')
+    assert(client.publish(1), '0/1', 'cookie')
+    assert(client.publish(2), '0/2', 'second cookie')
+    assert(client.outbox(), [
+        { cookie: '0/1', value: 1 },
+        { cookie: '0/2', value: 2 }
+    ], 'outbox is not empty')
+
+    client.published([
+        { cookie: '0/1', promise: '1/3' },
+        { cookie: '0/2', promise: '1/4' }
+    ])
+
+    client.receive([
+        { cookie: '0/1', promise: '1/3', value: 2, previous: '1/2' },
+    ])
+
+    var marker = client.receive([
+        { cookie: '1/1', promise: '1/2', value: 1, previous: '1/1' },
+        { cookie: '0/1', promise: '1/3', value: 2, previous: '1/2' },
+        { cookie: '0/2', promise: '1/4', value: 2, previous: '1/3' }
+    ])
+    client.receive([
+        { cookie: '0/2', promise: '1/4', value: 2, previous: '1/3' }
+    ])
+
+    assert(marker, '1/1', 'marker')
+
+    var array = []
+    client.each(marker, function (entry) {
+        array.push(entry)
+    })
+    assert(array, [
+        { cookie: '1/1', promise: '1/2', value: 1, previous: '1/1', uniform: true },
+        { cookie: '0/1', promise: '1/3', value: 2, previous: '1/2', uniform: true },
+        { cookie: '0/2', promise: '1/4', value: 2, previous: '1/3', uniform: true }
+    ], 'each')
+
+    assert(client.outbox(), [], 'outbox is empty after publshing')
+
+    client.publish(1)
+    client.publish(2)
+    client.publish(3)
+    assert(client.outbox(), [
+        { cookie: '0/3', value: 1 },
+        { cookie: '0/4', value: 2 },
+        { cookie: '0/5', value: 3 }
+    ], 'outbox for boundary')
+    client.publish(4)
+    client.publish(5)
+    client.publish(6)
+    client.published(null)
+    assert(client.outbox(), [{ cookie: '0/9', value: 0 }], 'boundary outbox will fail')
+    client.published(null)
+    assert(client.outbox(), [{ cookie: '0/a', value: 0 }], 'boundary outbox')
+    client.published([{ cookie: '0/a', promise: '1/9' }])
+    assert(client.outbox(), [], 'boundary outbox published')
+    client.receive([
+        { cookie: '0/3', promise: '1/5', value: 1, previous: '1/4' },
+        { cookie: '0/4', promise: '1/6', value: 2, previous: '1/5' },
+        { cookie: '0/5', promise: '1/7', value: 3, previous: '1/6' },
+        { cookie: '0/9', promise: '1/8', value: 0, previous: '1/7' }
+    ])
+    assert(client.outbox(), [], 'boundary outbox sent empty')
+    assert(client.sent.ordered, [], 'messages before boundary consumed')
+    assert(client.boundary != null, 'boundary exists')
+    assert(client.outbox(), [], 'boundary exists outbox')
+    client.receive([
+        { cookie: '0/a', promise: '1/9', value: 0, previous: '1/8' },
+    ])
+    assert(client.boundary == null, 'boundary cleared')
+    assert(client.outbox(), [
+        { cookie: '0/6', value: 4 },
+        { cookie: '0/7', value: 5 },
+        { cookie: '0/8', value: 6 }
+    ], 'boundary cleared outbox')
+    client.published(null)
+    assert(client.outbox(), [
+        { cookie: '0/b', value: 0 }
+    ], 'second boundary outbox')
+    client.published([{ promise: '1/d', cookie: '0/b' }])
+    client.publish(7)
+    client.publish(8)
+    client.publish(9)
+    client.receive([
+        { cookie: '0/6', promise: '1/a', value: 1, previous: '1/9' },
+        { cookie: '0/7', promise: '1/b', value: 2, previous: '1/a' },
+        { cookie: '0/0', promise: '2/0', value: 0, previous: '1/b' },
+    ])
+    assert(client.outbox(), [
+        { cookie: '0/8', value: 6 },
+        { cookie: '0/c', value: 7 },
+        { cookie: '0/d', value: 8 },
+        { cookie: '0/e', value: 9 }
+    ], 'second boundary resend outbox')
+    client.published(null)
+    assert(client.outbox(), [
+        { cookie: '0/f', value: 0 }
+    ], 'third boundary')
+    client.published([{ promise: '2/5', cookie: '0/f' }])
+    client.receive([
+        { cookie: '0/8', promise: '2/1', value: 6, previous: '2/0' },
+        { cookie: '0/c', promise: '2/2', value: 7, previous: '2/1' },
+        { cookie: '0/0', promise: '3/0', value: 0, previous: '2/2',
+            value: {
+                remap: [
+                    { was: '2/3', is: '3/1' },
+                    { was: '2/4', is: '3/2' },
+                    { was: '2/5', is: '3/3' }
+                ]
+            }
+        }
+    ])
+    client.receive([
+        { cookie: '0/d', promise: '3/1', value: 8, previous: '3/0' },
+        { cookie: '0/e', promise: '3/2', value: 9, previous: '3/1' },
+        { cookie: '0/f', promise: '3/3', value: 0, previous: '3/2' }
+    ])
+    assert(client.boundary == null, 'third bounary consumed')
+    client.publish(1)
+    client.publish(2)
+    client.publish(3)
+    assert(client.outbox(), [
+        { cookie: '0/10', value: 1 },
+        { cookie: '0/11', value: 2 },
+        { cookie: '0/12', value: 3 }
+    ], 'fourth outbox')
+    client.published([
+        { cookie: '0/10', promise: '3/6' },
+        { cookie: '0/11', promise: '3/7' },
+        { cookie: '0/12', promise: '3/8' }
+    ])
+    client.receive([
+        { cookie: '0/0', promise: '4/0', value: 0, previous: '3/3',
+            value: {
+                remap: [
+                    {  was: '3/5', is: '4/1' },
+                    {  was: '3/6', is: '4/2' },
+                    {  was: '3/7', is: '4/3' },
+                    {  was: '3/8', is: '4/4' }
+                ]
+            }
+        }
+    ])
+    client.receive([
+        { cookie: '8/10', promise: '4/1', value: 1, previous: '4/0' },
+        { cookie: '0/10', promise: '4/2', value: 1, previous: '4/1' },
+        { cookie: '0/0', promise: '5/0', value: 0, previous: '3/4',
+            value: {
+                remap: [
+                    {  was: '4/2', is: '5/1' },
+                    {  was: '4/3', is: '5/2' },
+                    {  was: '4/4', is: '5/3' }
+                ]
+            }
+        }
+    ])
+    assert(client.sent.ordered.length, 2, 'all remapped')
+    assert(client.outbox(), [], 'after remap, nothing to transmit')
+    client.receive([
+        { cookie: '0/11', promise: '5/1', value: 2, previous: '5/0' },
+        { cookie: '0/12', promise: '5/2', value: 3, previous: '5/1' }
+    ])
+    assert(client.sent.ordered.length, 2, 'all consumed')
+}
