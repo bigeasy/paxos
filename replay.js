d4284306a30dd12e0b2abbaf8773371125605083
Alan Gutierrez <alan@prettyrobots.com>
2015-12-01 01:39:19 -0600

    Remove dead code from `replay.js`.

--- replay.js
+++ replay.js
@@ -25,37 +25,21 @@ var legislator = new Legislator('0', {
     parliamentSize: 5,
     ping: 1,
     timeout: 2,
-    retry: 5,
-    recorder: function () {
-        recorded.push(JSON.parse(JSON.stringify(slice.call(arguments))))
-    }
+    retry: 5
 })
 
 var i = 0
 while (i < entries.length) {
     var entry = entries[i], vargs = JSON.parse(JSON.stringify(entry.vargs)), method = entry.method
-    if (method == 'prefer odd') {
-        legislator.prefer = function (citizen) {
-            return (+citizen) % 2
-        }
+    legislator[method].apply(legislator, vargs)
+    if (false) recorded.forEach(function (record) {
+        console.log('    ' + JSON.stringify(record))
+    })
+    while (recorded.length) {
+        console.log('< ' + JSON.stringify(entries[i]))
+        console.log('> ' + JSON.stringify(recorded[0]))
+        assert.deepEqual(recorded.shift(), entries[i], 'line ' + i)
         i++
-    } else if (method == 'prefer even') {
-        legislator.prefer = function (citizen) {
-            return (+citizen) % 2 == 0
-        }
-        i++
-    } else {
-        console.log(method, vargs)
-        legislator[method].apply(legislator, vargs)
-        if (false) recorded.forEach(function (record) {
-            console.log('    ' + JSON.stringify(record))
-        })
-        while (recorded.length) {
-            console.log('< ' + JSON.stringify(entries[i]))
-            console.log('> ' + JSON.stringify(recorded[0]))
-            assert.deepEqual(recorded.shift(), entries[i], 'line ' + i)
-            i++
-        }
     }
     console.log(i)
 }
