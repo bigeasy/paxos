21eb6b52f82189fe66c4a5831480eba0fa75fc2f
Alan Gutierrez <alan@prettyrobots.com>
2016-07-11 14:03:28 -0400

    Specific test for enqueue shared pulse.

---
+++ t/enqueue.t.js
@@ -0,0 +1,15 @@
+require('proof')(1, prove)
+
+function prove (assert) {
+    var Network = require('./network')
+    var network = new Network
+    network.addLegislators(5)
+    network.timeAndTick(6)
+    network.legislators[0].enqueue(network.time, 1, { value: 1 })
+    network.legislators[0].enqueue(network.time, 1, { value: 2 })
+    network.legislators[0].enqueue(network.time, 1, { value: 3 })
+    network.timeAndTick(1)
+    // Test that multiple messages will interleave their accepts and commits and
+    // ride the same pulse.
+    assert(network.legislators[0].log.max().value, { value: 3 }, 'enqueued')
+}
