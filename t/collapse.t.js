4420faca8f739bc5b31c3888b91adec9d2cdbecc
Alan Gutierrez <alan@prettyrobots.com>
2016-07-11 13:59:05 -0400

    Specific test for not enough reachable yet.

--- t/collapse.t.js
+++ t/collapse.t.js
@@ -1,4 +1,4 @@
-require('proof')(1, prove)
+require('proof')(2, prove)
 
 function prove (assert) {
     var Network = require('./network')
@@ -6,6 +6,9 @@ function prove (assert) {
     network.addLegislators(5)
     network.timeAndTick(6)
     network.legislators[0]._whenCollapse(network.time)
+    // Not enough proposals.
+    assert(network.legislators[0].consensus(network.time), null, 'not enough present')
+    // Run until recovered.
     network.timeAndTick(6)
     assert(network.legislators[0].government, {
         majority: [ '0', '1', '2' ],
