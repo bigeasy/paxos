a98ab5f217ed5246609b9d09069e71e294781de4
Alan Gutierrez <alan@prettyrobots.com>
2017-08-09 20:39:14 -0500

    Sketch of two-phase commit.

--- recorder.js
+++ recorder.js
@@ -1,4 +1,35 @@
-function Recorder () {
+// TODO Remember that the code is more complicated than the messaging. Let the
+// messages absorb some of the complexity of the code. (Divide them. Restrict
+// their structure.)
+
+function Recorder (listener) {
+    this._listener = listener
+    this._register = null
+}
+
+Recorder.prototype.request = function (request) {
+    var responses = []
+    for (var i = 0, request; (request = requests[i]) != null; i++) {
+        switch (request.method) {
+        case 'write':
+            this._register = {
+                promise: request.promise,
+                value: request.value
+            }
+            responses.push({ promise: request.promise, method: 'written' })
+            break
+        case 'commit':
+            if (message.promise == this._register.promise) {
+                var resgister = [ this._register, this._register = null ][0]
+                responses.push({ promise: request.promise, method: 'committed' })
+                this._listener.commit({
+                    promise: register.promise,
+                    value: register.value
+                })
+            }
+            break
+        }
+    }
 }
 
 module.exports = Recorder
--- writer.js
+++ writer.js
@@ -1,4 +1,50 @@
-function Writer () {
+function Writer (proposals) {
+    this._proposals = proposals
+    this._writing = []
+}
+
+Writer.prototype.nudge = function () {
+    if (this._writing.length == 0 && this._proposals.length != 0) {
+        // TODO So in order for it to be event driven it needs to go into a
+        // procession and then somewhere the sync needs to be added.
+    }
+}
+
+Writer.prototype._writeIf = function (condition, responses) {
+    if (this._proposals.length && condition) {
+        this._writing.push(this._proposals.shift())
+        responses.push({
+            to: this._quorum,
+            method: 'write',
+            promise: this._writing[0].promise
+        })
+    }
+}
+
+Writer.prototype.response = function (messages) {
+    var requests = []
+    for (var i = 0; i < messages.length; i++) {
+        var message = messages[i]
+        switch (message.method) {
+        case 'write':
+            if (message.promise == (this._writing[0] || {}).promise) {
+                requests.push({
+                    to: this._quorum,
+                    method: 'commit',
+                    promise: this._writing[0].promise
+                })
+            }
+            this._writeIf(!Monotonic.isBoundary(this._proposal.promise, 0), messages)
+            return responses
+        case 'commit':
+            var responses = []
+            this._writeIf(Monotonic.isBoundary(this._proposal.promise, 0), messages)
+            if (message.promise == this._writing[0].promise) {
+                this._writing.shift()
+            }
+            return responses
+        }
+    }
 }
 
 module.exports = Writer
