1d9190b7eacf0342bb18bc350d513f5d134e6181
Alan Gutierrez <alan@prettyrobots.com>
2017-09-01 01:47:13 -0500

    Purge dead code from test network.

--- t/network.js
+++ t/network.js
@@ -37,29 +37,6 @@ function Network () {
     this.time = 0
 }
 
-Network.prototype.receive = function (denizen, request) {
-    var responses = {}
-    request.to.forEach(function (id) {
-        var denizen = this.denizens[id]
-        if (this.failures[id] != 'isolate') {
-            responses[id] = this.denizens[id].request(this.time, request)
-        }
-        if (this.failures[id] == 'response' || this.failures[denizen.id] == 'isolate') {
-            responses[id] = null
-        }
-    }, this)
-    denizen.response(this.time, request, responses)
-}
-
-Network.prototype._send = function (denizen) {
-    var sent = false, message
-    while (denizen.shifter.peek()) {
-        this.receive(denizen, denizen.shifter.shift())
-        sent = true
-    }
-    return sent
-}
-
 Network.prototype.request = function (envelope) {
     envelope.responses[envelope.to] = this.denizens[envelope.to].request(this.time, envelope.request)
 }
@@ -67,17 +44,6 @@ Network.prototype.request = function (envelope) {
 Network.prototype.response = function (envelope) {
     if (Object.keys(envelope.responses).length == envelope.request.message.to.length) {
         this.denizens[envelope.from].response(this.time, envelope.request, envelope.responses)
-    }
-}
-
-function getFailure (failure, request) {
-    switch (typeof failure) {
-    case 'string':
-        return failure
-    case 'function':
-        return failure(request)
-    default:
-        return 'none'
     }
 }
 
@@ -156,103 +122,6 @@ Network.prototype.intercept = function () {
     return intercepted
 }
 
-Network.prototype.send2 = function () {
-    var vargs = Array.prototype.slice.call(arguments)
-    var count = typeof vargs[0] == 'number' ? vargs.shift() : Infinity
-    var sent = true
-    while (sent && count--) {
-        var requests = this.getRequests.apply(this, vargs)
-        sent = false
-        for (var id in requests) {
-            while (requests[id].length) {
-                sent = true
-                var request = requests[id].shift()
-                switch (getFailure(this.failures[request.to])) {
-                case 'skip':
-                    break
-                case 'response':
-                    this.request(request)
-                case 'isolate':
-                    request.responses[request.to] = null
-                    this.response(request)
-                    break
-                case 'none':
-                    this.request(request)
-                    this.response(request)
-                    break
-                }
-            }
-        }
-    }
-}
-
-Network.prototype.send = function () {
-    var vargs = Array.prototype.slice.call(arguments)
-    var count = typeof vargs[0] == 'number' ? vargs.shift() : Infinity
-    var denizens = vargs.length == 0
-                 ? this.denizens
-                 : vargs.shift().map(function (id) { return this.denizens[id] }.bind(this))
-    var ticked = true
-    while (ticked && count--) {
-        ticked = false
-        for (var i = 0, denizen; (denizen = denizens[i]) != null; i++) {
-            if (this.failures[denizen.id] != 'isolate') {
-                denizen.scheduler.check(this.time)
-                while (this._send(denizen)) {
-                    ticked = true
-                }
-            }
-        }
-    }
-}
-
-Network.prototype.getRequests = function () {
-    var vargs = Array.prototype.slice.call(arguments)
-    if ((typeof (vargs[0] || '')) != 'object') {
-        vargs.unshift({})
-    }
-    var response = vargs.shift()
-    if (vargs.filter(function (id) { return /^\d+$/.test(id) }).length == 0) {
-        vargs.push.apply(vargs, Object.keys(this.denizens))
-    }
-    if (vargs.filter(function (id) { return ! /^\d+$/.test(id) }).length == 0) {
-        vargs.push('events', 'outbox')
-    }
-    var requests = Array.isArray(response) ? {} : response
-    vargs.filter(function (id) {
-        return this.denizens[id]
-    }.bind(this)).map(function (id) {
-        return this.denizens[id]
-    }.bind(this)).forEach(function (denizen) {
-        if (~vargs.indexOf('events')) {
-            denizen.scheduler.check(this.time)
-        }
-        var request
-        requests[denizen.id] = Array.isArray(response) ? response : []
-        if (~vargs.indexOf('outbox')) {
-            while ((request = denizen.shifter.shift()) != null) {
-                var responses = {}
-                request.message.to.forEach(function (to) {
-                    requests[denizen.id].push({
-                        request: request,
-                        to: to,
-                        from: denizen.id,
-                        responses: responses
-                    })
-                })
-            }
-        }
-    }.bind(this))
-    return response
-}
-
-Network.prototype.timeAndTick = function (count) {
-    while (count-- != 0) {
-        this.time++
-        this.tick()
-    }
-}
-
 Network.prototype.push = function () {
     var id = String(this.denizens.length)
     var denizen = new Paxos(this.time, 1, id, {
@@ -276,10 +145,6 @@ Network.prototype.populate = function (count) {
             this.denizens[0].immigrate(this.time, 1, denizen.id, denizen.cookie, { location: denizen.id })
         }
     }
-}
-
-Network.prototype.isolate = function (id) {
-    this.failures[id] = 'isolate'
 }
 
 module.exports = Network
