aa88e7aa8d61836643cc66b6fc7605c220ba4c96
Alan Gutierrez <alan@prettyrobots.com>
2020-02-12 15:16:11 -0600

    Convert Shaper to ES6 class.
    See #461.

--- shaper.js
+++ shaper.js
@@ -60,220 +60,222 @@ var assert = require('assert')
 // TODO Stop saying "quorum" when you mean "seats filled."
 
 //
-function Shaper (parliamentSize, government, recovered) {
-    this._parliamentSize = government.majority.length * 2 - 1
-    this._shouldExpand = parliamentSize != this._parliamentSize
-    this._government = government
-    this._seen = {}
-    this._expandable = []
-    this._arriving = []
-    this.decided = false
-    this._governments = []
-    this.outbox = {}
-    this._representative = null
-    this._shouldNaturalize = this._government.majority.length +
-        this._government.minority.length +
-        this._government.constituents.length != this._government.acclimated.length
-    this._shouldRecover(recovered) || this._shouldContract()
-}
-
-Shaper.prototype._shouldRecover = function (recovered) {
-    if (recovered) {
-        this._governments.push({
-            quorum: this._government.majority,
-            government: {
-                majority: this._government.majority,
-                minority: this._government.minority
-            }
-        })
-    }
-}
-
-Shaper.prototype._shouldContract = function () {
-    if (
-        this._government.promise != '0/0' &&
-        this._government.majority.length + this._government.minority.length != this._parliamentSize
-    ) {
-        var majority = this._government.majority.slice()
-        var minority = this._government.minority.slice()
-        minority.unshift(majority.pop())
-        var demote = minority.pop()
-        this._governments.push({
-            quorum: this._government.majority,
-            government: {
-                majority: majority,
-                minority: minority,
-                demote: demote
-            }
-        })
-    }
-}
-
-Shaper.prototype.unreachable = function (unreachable) {
-    if (this.decided) {
-        return null
+class Shaper {
+    constructor (parliamentSize, government, recovered) {
+        this._parliamentSize = government.majority.length * 2 - 1
+        this._shouldExpand = parliamentSize != this._parliamentSize
+        this._government = government
+        this._seen = {}
+        this._expandable = []
+        this._arriving = []
+        this.decided = false
+        this._governments = []
+        this.outbox = {}
+        this._representative = null
+        this._shouldNaturalize = this._government.majority.length +
+            this._government.minority.length +
+            this._government.constituents.length != this._government.acclimated.length
+        this._shouldRecover(recovered) || this._shouldContract()
     }
 
-    var id = this._government.arrived.id[unreachable]
-    assert(id != null, 'unable to determine unreachable id')
-
-    // Depart any unreachable islanders.
-    return this._governments.shift() || {
-        quorum: this._government.majority,
-        government: {
-            majority: this._government.majority,
-            minority: this._government.minority.filter(function ($id) { return $id != id }),
-            departed: id
+    _shouldRecover (recovered) {
+        if (recovered) {
+            this._governments.push({
+                quorum: this._government.majority,
+                government: {
+                    majority: this._government.majority,
+                    minority: this._government.minority
+                }
+            })
         }
     }
-}
 
-Shaper.prototype.acclimate = function (promise) {
-    if (this.decided) {
-        return null
-    }
-
-    var government = this._government
-
-    var id = government.arrived.id[promise]
-    assert(id != null, 'unable to determine acclimate id')
-
-
-    assert(!~government.acclimated.indexOf(id), 'already acclimated')
-
-    return this._governments.shift() || {
-        quorum: this._government.majority,
-        government: {
-            majority: this._government.majority,
-            minority: this._government.minority,
-            acclimate: id
-        }
-    }
-}
-
-// `Shaper.update` determines if a new government should be created that has a
-// new shape. Note that embark and arrive takes place is elsewhere.
-
-//
-Shaper.prototype.acclimated = function (id) {
-    if (this.decided) {
-        return null
-    }
-
-    // We're not going to return an expanded government until we get two
-    // expandable entries so if we have a contraction it will go the first time
-    // acclimated is called.
-    if (~this._government.majority.indexOf(id)) {
-        // Majority members are not our responsibility. They trigger their own
-        // collapse.
-    } else if (this._shouldExpand && !~this._government.minority.indexOf(id)) {
-        this._expandable.push(id)
-        // TODO Is the quorum the new majority or the old majority?
-        // TODO Think about growth commit race conditions again.
-
-        //
-        if (this._expandable.length == 2) {
-            // We should expand and we have islanders who can be appointed to
-            // the government so let's grow the government.
+    _shouldContract () {
+        if (
+            this._government.promise != '0/0' &&
+            this._government.majority.length + this._government.minority.length != this._parliamentSize
+        ) {
             var majority = this._government.majority.slice()
             var minority = this._government.minority.slice()
-            var promote = [ this._expandable.shift(), this._expandable.shift() ]
-            minority.push.apply(minority, promote)
-            majority.push(minority.shift())
-            return {
-                quorum: majority,
+            minority.unshift(majority.pop())
+            var demote = minority.pop()
+            this._governments.push({
+                quorum: this._government.majority,
                 government: {
                     majority: majority,
                     minority: minority,
-                    promote: promote
+                    demote: demote
                 }
+            })
+        }
+    }
+
+    unreachable (unreachable) {
+        if (this.decided) {
+            return null
+        }
+
+        var id = this._government.arrived.id[unreachable]
+        assert(id != null, 'unable to determine unreachable id')
+
+        // Depart any unreachable islanders.
+        return this._governments.shift() || {
+            quorum: this._government.majority,
+            government: {
+                majority: this._government.majority,
+                minority: this._government.minority.filter(function ($id) { return $id != id }),
+                departed: id
             }
         }
     }
 
-    return this._governments.shift() || this._arrival() || null
-}
-
-Shaper.prototype.arrived = function (id) {
-    assert(this._arriving.length > 0 && id == this._arriving[0].id)
-    this._arriving.shift()
-}
-
-// TODO This seems really broken to me. (It is not. Read the following
-// description and then the description in body of `embark` and then amalgamate
-// the two of them.)
-//
-// Looks broken, doesn't it? Updating the cookie and properties of an existing
-// record makes think that there is a race condition where the leader is going
-// to have a different version of the arrival message from the other majority
-// members if `embark` is called and the arrival is the `0` entry in the array.
-//
-// However, we're not using the entry in the array. Our arrival government is
-// an object created in `_arrival`. Calling `embark` for the `0` arrival is a
-// missed update if `_arrival` has been called to create an arrival government
-// from the entry. If the call to `embark` provided a different cookie, it's a
-// miss. The wrong cookie will arrive. We'll have to wait for the arrival and
-// subsequent departure followed by that higher-level departure detection that
-// will trigger a restart.
-//
-// The notes below still stand. The make sense having re-read them.
-//
-Shaper.prototype.embark = function (arrival) {
-    // We do not going to reject a duplicate embarkation for a particular id.
-    //
-    // Here is a race condition and how it will shake itself out.
-    //
-    // We could be in the middle of arriving a particular islander id when the
-    // islander crashed restarts and submits a new cookie. We will update the
-    // cookie here, but that's not going to be the same as the cookie that got
-    // written into the log.
-    //
-    // We can make it a general case that if the cookies mismatch
-    // every one is very disappointed. Thus, we can catch this on sync.
-    for (var i = 0, I = this._arriving.length; i < I; i++) {
-        if (this._arriving[i].id == arrival.id) {
-            break
+    acclimate (promise) {
+        if (this.decided) {
+            return null
         }
-    }
-    if (i == this._arriving.length) {
-        this._arriving.push(arrival)
-    } else {
-        this._arriving[i].properties = arrival.properties
-        this._arriving[i].cookie = arrival.cookie
-        this._arriving[i].acclimated = arrival.acclimated
-    }
 
-    // Do nothing if our container indicates that a decision has been reached.
-    return this.decided ? null : this._arrival()
-}
+        var government = this._government
 
-// Generate an arrival government if we have an arrival available.
+        var id = government.arrived.id[promise]
+        assert(id != null, 'unable to determine acclimate id')
 
-//
-Shaper.prototype._arrival = function () {
-    if (this._arriving.length) {
-        var arrival = this._arriving[0]
-        return {
+
+        assert(!~government.acclimated.indexOf(id), 'already acclimated')
+
+        return this._governments.shift() || {
             quorum: this._government.majority,
             government: {
                 majority: this._government.majority,
                 minority: this._government.minority,
-                arrive: {
-                    id: arrival.id,
-                    properties: arrival.properties,
-                    cookie: arrival.cookie
-                },
-                acclimate: arrival.acclimated ? arrival.id : null
+                acclimate: id
             }
         }
     }
-    return null
-}
 
-Shaper.null = {
-    unreachable: function () { return null },
-    acclimate: function () { return null },
-    _arriving: []
+    // `Shaper.update` determines if a new government should be created that has a
+    // new shape. Note that embark and arrive takes place is elsewhere.
+
+    //
+    acclimated (id) {
+        if (this.decided) {
+            return null
+        }
+
+        // We're not going to return an expanded government until we get two
+        // expandable entries so if we have a contraction it will go the first time
+        // acclimated is called.
+        if (~this._government.majority.indexOf(id)) {
+            // Majority members are not our responsibility. They trigger their own
+            // collapse.
+        } else if (this._shouldExpand && !~this._government.minority.indexOf(id)) {
+            this._expandable.push(id)
+            // TODO Is the quorum the new majority or the old majority?
+            // TODO Think about growth commit race conditions again.
+
+            //
+            if (this._expandable.length == 2) {
+                // We should expand and we have islanders who can be appointed to
+                // the government so let's grow the government.
+                var majority = this._government.majority.slice()
+                var minority = this._government.minority.slice()
+                var promote = [ this._expandable.shift(), this._expandable.shift() ]
+                minority.push.apply(minority, promote)
+                majority.push(minority.shift())
+                return {
+                    quorum: majority,
+                    government: {
+                        majority: majority,
+                        minority: minority,
+                        promote: promote
+                    }
+                }
+            }
+        }
+
+        return this._governments.shift() || this._arrival() || null
+    }
+
+    arrived (id) {
+        assert(this._arriving.length > 0 && id == this._arriving[0].id)
+        this._arriving.shift()
+    }
+
+    // TODO This seems really broken to me. (It is not. Read the following
+    // description and then the description in body of `embark` and then amalgamate
+    // the two of them.)
+    //
+    // Looks broken, doesn't it? Updating the cookie and properties of an existing
+    // record makes think that there is a race condition where the leader is going
+    // to have a different version of the arrival message from the other majority
+    // members if `embark` is called and the arrival is the `0` entry in the array.
+    //
+    // However, we're not using the entry in the array. Our arrival government is
+    // an object created in `_arrival`. Calling `embark` for the `0` arrival is a
+    // missed update if `_arrival` has been called to create an arrival government
+    // from the entry. If the call to `embark` provided a different cookie, it's a
+    // miss. The wrong cookie will arrive. We'll have to wait for the arrival and
+    // subsequent departure followed by that higher-level departure detection that
+    // will trigger a restart.
+    //
+    // The notes below still stand. The make sense having re-read them.
+    //
+    embark (arrival) {
+        // We do not going to reject a duplicate embarkation for a particular id.
+        //
+        // Here is a race condition and how it will shake itself out.
+        //
+        // We could be in the middle of arriving a particular islander id when the
+        // islander crashed restarts and submits a new cookie. We will update the
+        // cookie here, but that's not going to be the same as the cookie that got
+        // written into the log.
+        //
+        // We can make it a general case that if the cookies mismatch
+        // every one is very disappointed. Thus, we can catch this on sync.
+        for (var i = 0, I = this._arriving.length; i < I; i++) {
+            if (this._arriving[i].id == arrival.id) {
+                break
+            }
+        }
+        if (i == this._arriving.length) {
+            this._arriving.push(arrival)
+        } else {
+            this._arriving[i].properties = arrival.properties
+            this._arriving[i].cookie = arrival.cookie
+            this._arriving[i].acclimated = arrival.acclimated
+        }
+
+        // Do nothing if our container indicates that a decision has been reached.
+        return this.decided ? null : this._arrival()
+    }
+
+    // Generate an arrival government if we have an arrival available.
+
+    //
+    _arrival () {
+        if (this._arriving.length) {
+            var arrival = this._arriving[0]
+            return {
+                quorum: this._government.majority,
+                government: {
+                    majority: this._government.majority,
+                    minority: this._government.minority,
+                    arrive: {
+                        id: arrival.id,
+                        properties: arrival.properties,
+                        cookie: arrival.cookie
+                    },
+                    acclimate: arrival.acclimated ? arrival.id : null
+                }
+            }
+        }
+        return null
+    }
+
+    static null = {
+        unreachable: function () { return null },
+        acclimate: function () { return null },
+        _arriving: []
+    }
 }
 
 module.exports = Shaper
