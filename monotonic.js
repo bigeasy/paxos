e994bd92f0d7d26c5537260a6cbe1a6dec3354e1
Alan Gutierrez <alan@prettyrobots.com>
2021-10-08 05:32:11 -0500

    Compare abitrary length promises.

--- monotonic.js
+++ monotonic.js
@@ -1,10 +1,22 @@
-const ascension = require('ascension')
 const whittle = require('whittle')
 
 const Monotonic = {
     toBigInt: promise => promise.split('/').map(part => BigInt(`0x${part}`)),
-    compare: whittle(ascension([ BigInt, BigInt ]), promise => Monotonic.toBigInt(promise)),
+    compare: whittle(function (left, right) {
+        for (let i = 0, I = Math.min(left.length, right.length); i < I; i++) {
+            const compare = (left[i] > right[i]) - (left[i] < right[i])
+            if (compare != 0) {
+                return compare
+            }
+        }
+        return left.length - right.length
+    }, promise => Monotonic.toBigInt(promise)),
     isGovernment: promise => promise.endsWith('/0'),
+    incrementr: (promise, index) => {
+        const split = Monotonic.toBigInt(promise).reverse()
+        split[index] += 1n
+        return split.map(part => part.toString(16)).join('/')
+    },
     increment: (promise, index) => {
         const split = Monotonic.toBigInt(promise)
         split[index] += 1n
